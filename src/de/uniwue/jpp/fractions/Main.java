9ac3bb1e2169dfb05d946cf68c62f90033728687
chris <c.skwirblies@googlemail.com>
2018-02-12 13:05:02 +0100

    task

--- src/de/uniwue/jpp/fractions/Main.java
+++ src/de/uniwue/jpp/fractions/Main.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 public  class Main {
     public static void Main (String[] args){
         String input;
+        System.out.println("Put in your fraction");
         Scanner scanInput = new Scanner(System.in);
         input = scanInput.nextLine();
         scanInput.close();
+        Parser.parse(input);
+
     }
 }
 
